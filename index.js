7eeda7ad0613c071f2b749fcd694fdc0b4190c2e
Codename: Steeve Knight <steeve@dharmiweb.net>
2018-09-11 16:04:10 -0400

    tweaks; prep test/scale;

--- index.js
+++ index.js
@@ -8,14 +8,44 @@
 const randInt = require('random-int');
 const calcCentersOfMass = require('./calc-centers-of-mass');
 
+// grid factories
+const grids = {
+    random(cells, max) {
+        const grid = [];
+        for (y = 0; y < cells; y++) {
+            grid[y] = [];
+            for (x = 0; x < cells; x++) {
+                grid[y][x] = randInt(0, max);
+            }
+        }
+        return grid;
+    },
+    test01() {
+        const grid = [];
+        const cells = 7;
+
+        for (y = 0; y < cells; y++) {
+            grid[y] = [];
+            for (x = 0; x < cells; x++) {
+                grid[y][x] = 242;
+            }
+        }
+
+        return grid;
+    }
+};
+
+const threshold = 200;
+const grid = grids.random(7, threshold + 60);
+
 /**
- * This generator simplifies a state-safe context for interating the full-grid
+ * This generator simplifies a state-safe context for interating the full-grid with stops & starts
  * 
  * @param {Int} min 
  * @param {Array} grid 
  * @param {Array} seen 
  */
-function* findNextGridCellAboveThreshold(min, grid, seen) {
+const findNextGridCellAboveThreshold = function* (min, grid, seen) {
     for (const y in grid) {
         for (const x in grid[y]) {
             if (grid[y][x] > min && !seen.includes(x + '.' + y)) {
@@ -23,19 +53,6 @@ function* findNextGridCellAboveThreshold(min, grid, seen) {
                 yield { x: +x, y: +y };
             }
         }
-    }
-}
-
-const threshold = 200;
-const max = threshold + 60;
-
-const grid = [];
-const cells = 7;
-
-for (y = 0; y < cells; y++) {
-    grid[y] = [];
-    for (x = 0; x < cells; x++) {
-        grid[y][x] = randInt(0, max);
     }
 }
 
@@ -66,20 +83,20 @@ const getAdjacentCellsMap = ({ x, y }) => [
 ];
 
 /**
- * recursor for hunting down sub-regions of adjacent cells, each with a value above a threshold value
+ * recursor for hunting down sub-regions of adjacent cells, each with a value above a threshold
  * 
  * @param {Array} xy 
+ * @todo Y-Combinate/Memoize
  */
 const findAdjacentCellsAboveThreshold = (xy) => {
     let token;
-
     for (test of getAdjacentCellsMap(xy)) {
         if (grid[test.y] && grid[test.y][test.x] && grid[test.y][test.x] > threshold) {
             token = test.x + '.' + test.y;
             if (!seen.includes(token)) {
                 seen.push(token);
                 subRegions[srIdx].push({ x: test.x, y: test.y, v: grid[test.y][test.x] });
-                findAdjacentCellsAboveThreshold([test.x, test.y]);
+                findAdjacentCellsAboveThreshold({ x: test.x, y: test.y });
             }
         }
 
